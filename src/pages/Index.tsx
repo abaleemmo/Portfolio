5d506b559fab8db02d7c40eda019e2771f0dc3d7
[dyad] <abaleemmohammed@gmail.com>
2025-08-16 19:12:50 -0500

    [dyad] wrote 1 file(s), deleted 1 file(s)

--- src/pages/Index.tsx
+++ src/pages/Index.tsx
@@ -1,14 +1,14 @@
 import React from 'react';
 import Layout from '@/components/Layout';
 import HeroSection from '@/components/HeroSection';
-import { MadeWithDyad } from "@/components/made-with-dyad";
+// import { MadeWithDyad } from "@/components/made-with-dyad"; // Removed import
 
 const Index = () => {
   return (
     <Layout>
       <HeroSection />
       {/* Future sections like About, Projects, Contact will go here */}
-      <MadeWithDyad />
+      {/* <MadeWithDyad /> Removed component */}
     </Layout>
   );
 };
