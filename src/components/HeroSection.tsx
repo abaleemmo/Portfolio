692b0d52dcf041947671c64b8a28efc5d1d385a8
[dyad] <abaleemmohammed@gmail.com>
2025-08-19 20:32:05 -0500

    [dyad] wrote 1 file(s) + extra files edited outside of Dyad

--- src/components/HeroSection.tsx
+++ src/components/HeroSection.tsx
@@ -4,7 +4,7 @@ import { Link } from 'react-router-dom';
 
 const HeroSection = () => {
   return (
-    <section className="relative w-full py-12 md:py-24 lg:py-32 xl:py-48 bg-gradient-to-r from-blue-50 to-indigo-50 dark:from-gray-900 dark:to-gray-800">
+    <section className="relative w-full py-10 md:py-20 lg:py-28 xl:py-36 bg-gradient-to-r from-blue-50 to-indigo-50 dark:from-gray-900 dark:to-gray-800">
       <div className="container px-4 md:px-6 text-center">
         <div className="flex flex-col items-center space-y-4">
           {/* Monogram image adjusted to be 75% width of the text */}
@@ -17,14 +17,7 @@ const HeroSection = () => {
           <p className="mx-auto max-w-[700px] text-gray-600 md:text-xl dark:text-gray-300">
             A passionate high school student exploring the world of technology and creativity.
           </p>
-          <div className="space-x-4 pt-4">
-            <Link to="/projects">
-              <Button size="lg" className="bg-blue-600 hover:bg-blue-700 text-white">View My Work</Button>
-            </Link>
-            <Link to="/contact">
-              <Button size="lg" variant="outline" className="border-blue-600 text-blue-600 hover:bg-blue-50 dark:border-blue-400 dark:text-blue-400 dark:hover:bg-gray-700">Get in Touch</Button>
-            </Link>
-          </div>
+          {/* Removed the "View My Work" and "Get in Touch" buttons */}
         </div>
       </div>
     </section>
